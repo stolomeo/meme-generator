1c9bf38b5b81ac338fc4eca782c2cbc7c9fc6663
stolomeo <tolomeosebastian@gmail.com>
2022-04-24 22:12:28 -0400

    Finish Content component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import Header from "./components/Header";
+import Content from "./components/Content";
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Content />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Content.js
@@ -0,0 +1,12 @@
+export default function Content() {
+  return (
+    <main>
+      <form>
+        <input type="text" className="main-form-input" />
+        <input type="text" className="main-form-input" />
+        <button className="main-form-btn">Get a new meme image 🖼</button>
+      </form>
+      <img src="#" alt="meme" className="main-img-meme" />
+    </main>
+  );
+}
