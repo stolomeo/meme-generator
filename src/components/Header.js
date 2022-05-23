a4833f984837661d6f6bd50afb9fa15e44027c93
stolomeo <tolomeosebastian@gmail.com>
2022-05-23 00:25:47 -0400

    Reordered

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,18 +11,17 @@ export default function Header() {
 }
 
 const HeaderWrapper = styled.header`
-  display: flex;
-  font-family: "Koulen", cursive;
   align-items: center;
-  height: 65px;
+  background: linear-gradient(90deg, #672280 1.18%, #a626d3);
+  box-shadow: 0 50px 100px -20px rgb(50 50 93 / 25%),
+    0 30px 60px -30px rgb(0 0 0 / 30%), inset 0 -2px 6px 0 rgb(10 37 64 / 35%);
+  color: #fff;
+  display: flex;
+  font-family: Koulen, cursive;
   font-size: 24px;
-  background: linear-gradient(90deg, #672280 1.18%, #a626d3 100%);
-  color: white;
-  padding: 20px;
+  height: 65px;
   justify-content: center;
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
-    rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
-    rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+  padding: 20px;
 `;
 
 const Logo = styled.img`
